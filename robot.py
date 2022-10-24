67fd8d9b00b74890ff254e9117508b93919f926f
s183898 <59825652+s183898@users.noreply.github.com>
2022-10-24 21:21:21 +0200

    google searches

--- robot.py
+++ robot.py
@@ -1,4 +1,5 @@
 import urllib3
+from bs4 import BeautifulSoup
 
 class Scraper:
     def __init__(self, input_url):
@@ -9,26 +10,25 @@ class Scraper:
     def get_html(self, url):
         http = urllib3.PoolManager()
         r = http.request('GET', url)
-        return r.data.decode('utf-8')
+        return r.data.decode("unicode_escape")
 
     def get_links(self, url):
-        links = []
         html = self.get_html(url)
-        html = html.split('\n')
+        soup = BeautifulSoup(html, 'html.parser')
+        links = []
 
-        for line in html:
-            if '<a href=' in line:
-                link = line.split('"')[1]
-                if link not in links:
+        for line in soup.find_all('a'):
+            link = line.get('href')
+            if link != "" and link != "#" and link != None:
+                if self.url in link and link != url:
                     links.append(link)
-        
+                if "https://" not in link:
+                    links.append("https://"+self.url + link)
         return links
-    
+
     def get_robot_txt(self):
         robot_url = self.url + '/robots.txt'
-        http = urllib3.PoolManager()
-        r = http.request('GET', robot_url)
-        return r.data.decode('utf-8')
+        return self.get_html(robot_url)
 
     def root_url(self):
         root_url = self.input_url.split('//')[1]
@@ -41,37 +41,8 @@ class Scraper:
         else:
             return False
 
-#### Zarpaulo ####
-
-# zarpaulo = Scraper('https://www.zarpaulo.com/dfsfdsf')
-
-# url = zarpaulo.url
-
-# links = zarpaulo.get_links(url)
-
-# for link in links:
-#     print(zarpaulo.get_links(url+"/"+link))
-
-#### Lydmor ####
-
-# lydmor = Scraper("https://www.lydmor.dk/")
-
-# url = lydmor.url
-
-# links = lydmor.get_links(url)
-
-# print(links)
-# print()
-# print(lydmor.get_robot_txt())
-
-
-#### facebook ####
-
-link = "https://www.facebook.com/robots.txt"
-
-facebook = Scraper(link)
-
-txt = facebook.get_robot_txt()
-print(txt)
-print(facebook.search_keyword("event", txt))
-
+if __name__ == "__main__":
+    link = 'https://lydmor.dk'
+    scraper = Scraper(link)
+    links = scraper.get_links(link)
+    print(links)
--- search.py
+++ search.py
@@ -1,6 +1,45 @@
-from googlesearch import search
+import requests
+import urllib
+import pandas as pd
+from requests_html import HTML
+from requests_html import HTMLSession
 
-query = "events near me"
+from robot import Scraper
 
-for j in search(query, tld="co.in", num=10, stop=10, pause=2):
-    print(j)
+def get_source(url):
+    try:
+        session = HTMLSession()
+        response = session.get(url)
+        return response
+    except requests.exceptions.RequestException as e:
+        print(e)
+
+def google_search(query):
+
+    query = urllib.parse.quote_plus(query)
+    response = get_source("https://www.google.co.uk/search?q=" + query)
+
+    links = list(response.html.absolute_links)
+    google_domains = ('https://www.google.', 
+                      'https://google.', 
+                      'https://webcache.googleusercontent.', 
+                      'http://webcache.googleusercontent.', 
+                      'https://policies.google.',
+                      'https://support.google.',
+                      'https://maps.google.',
+                      'https://translate.google')
+
+    for url in links[:]:
+        if url.startswith(google_domains):
+            links.remove(url)
+
+    return links
+
+searches = google_search("events near me")
+
+print(searches)
+
+for search in searches:
+    scraper = Scraper(search)
+    links = scraper.get_html(search)
+    print(links)
